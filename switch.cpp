1a9e0d2d1f810dc806c1470ee741976d90499779
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-25 16:48:52 +0600

    added more about switch case

--- switch.cpp
+++ switch.cpp
@@ -32,5 +32,17 @@ int main()
         cout << "Not a day\n";
     }
 
+
+    //or if you want to check with and condition you have to write down it the switch
+    // switch case is used mostly if any condition is satisfied then it will work for the rest case in it.
+    // N.B: no need to write the break in that case;
+    switch(day % 2) // here possible case is 1 or 0: 1 means reminder will be 1 and 0 means reminder will be  0 means the number is even
+    {
+        case 1:
+            cout << "odd day\n";
+            break;
+        case 0:
+        cout << "even day\n";
+    }
     return 0;
 }
