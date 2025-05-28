db3e4c0a54409802024d8b1088f3bb77e00e6602
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-28 07:17:50 +0600

    added solution of taking string with spaces

---
+++ Module 3/constructor.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//declaring class
+class Student{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+};
+int main(){
+    Student rahim;
+    return 0;
+}
